d37a4d47227152107489a18778aa256c559d6dc9
masterofcontroversy <masterofcontroversy@mail.com>
2021-12-14 22:38:26 -0500

    rewritten for no pushes

--- FE8_BattleCritCap/CritCap.s
+++ FE8_BattleCritCap/CritCap.s
@@ -1,9 +1,9 @@
 .thumb
 
 @called from 2ACA4
-push {r14}
-mov     r5,r2
-add     r5,#0x6A
+mov     r5, r2
+add     r5, #0x6A
+mov     r4, #0x0
 
 @Check if crit is greater than 100
 cmp r1, #100
@@ -11,8 +11,8 @@ blt End
 mov     r1, #100 @Make crit 100 if it's greater than 100
 End:
 strh    r1, [r5]
-mov r4, #0x0
-mov     r0,r2
-add     r0,#0x48
-pop {r3}
+ldr     r3,=#0x802ACAC|1
 bx r3
+
+.align
+.ltorg
