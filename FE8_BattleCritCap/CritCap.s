e17d4573d08e70ef4a7f313ae2bf71147dc0f112
masterofcontroversy <masterofcontroversy@mail.com>
2021-12-14 21:24:28 -0500

    Fixed crit always being 0 if not over 100

--- FE8_BattleCritCap/CritCap.s
+++ FE8_BattleCritCap/CritCap.s
@@ -7,9 +7,12 @@ add     r5,#0x6A
 
 @Check if crit is greater than 100
 cmp r1, #100
-blt Continue
+blt End
 mov     r1, #100 @Make crit 100 if it's greater than 100
-strh    r1,[r5]
-
-pop {r0}
-bx r0
+End:
+strh    r1, [r5]
+mov r4, #0x0
+mov     r0,r2
+add     r0,#0x48
+pop {r3}
+bx r3
