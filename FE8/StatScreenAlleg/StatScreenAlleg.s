8dac1886ea8982070339eebb709a37a4e0519815
masterofcontroversy <masterofcontroversy@mail.com>
2022-10-23 02:58:46 -0400

    Fix StatScreenAlleg using other palette in world map. Update SSBlue Palette

--- FE8/StatScreenAlleg/StatScreenAlleg.s
+++ FE8/StatScreenAlleg/StatScreenAlleg.s
@@ -1,24 +1,22 @@
 .thumb
 
+.macro blh to, reg
+    ldr \reg, =\to
+    mov lr, \reg
+    .short 0xF800
+.endm
+
 .equ EnemyPal, PlayerPal+4
 .equ NPCPal,   PlayerPal+8
 .equ OtherPal, PlayerPal+12
 
-.equ GetUnit,       0x8019431
-.equ gGameState,    0x202BCB0
-.equ gActiveUnitId, 0x202BE44
-.equ gUnitMap,      0x202E4D8
+.equ gpStatScreenUnit, 0x2003C08
 
 @fetches the appropriate palette based on unit allegiance and returns to 80885a4
 
-ldr  r2,=gGameState
-ldrh r0,[r2,#0x14] @xcoord
-ldrh r1,[r2,#0x16] @ycoord
-bl   GetUnitFromCoords
-cmp  r0,#0
-bne  CheckAlleg
-ldr  r1,=gActiveUnitID
-ldrb r0,[r1]
+ldr  r1, =gpStatScreenUnit
+ldr  r1,[r1]
+ldrb r0,[r1,#0xB]
 
 CheckAlleg:
 mov  r1,#0xC0
@@ -50,16 +48,6 @@ lsl  r1,#1
 mov  r2,#0x80
 ldr  r3,=0x80885a4|1
 bx   r3
-
-GetUnitFromCoords:
-@gets deployment number, given r0=x and r1=y
-ldr  r2,=gUnitMap
-ldr  r2,[r2]
-lsl  r1,#2 @y*4
-add  r1,r2 @row address
-ldr  r1,[r1]
-ldrb r0,[r1,r0]
-bx   lr
 
 .align
 .ltorg
